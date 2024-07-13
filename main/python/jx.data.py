b144a75392f5a9e7aee177370bc7bf8defde0a0a
Losketch <140333245+Losketch@users.noreply.github.com>
2024-07-13 21:44:25 +0800

    Add files via upload

---
+++ main/python/jx.data.py
@@ -0,0 +1,32 @@
+import re
+import os
+
+current_directory = os.getcwd()
+UData = os.path.join(current_directory, 'assets', 'UData.txt')
+
+with open(UData, 'r', encoding='utf-8') as f, open('assets/UnicodeData.txt', 'w', encoding='utf-8') as out:
+
+    start = end = None
+
+    for line in f:
+        code_point = int(line.split(';')[0], 16)
+
+        if "First" in line:
+            start = code_point
+            desc = re.sub(r'[<>,]| First', '', line.split(';')[1])
+
+        elif "Last" in line:
+            end = code_point
+            desc = re.sub(r'[<>,]| Last', '', line.split(';')[1])
+
+            if start and end:
+                for cp in range(start, end+1):
+                    out.write(f'"U+{cp:04X}","{desc}-{cp:04X}"\n')
+                start = end = None
+
+        else:
+            desc = line.split(';')[1]
+            if desc:
+                out.write(f'"U+{code_point:04X}","{desc}"\n')
+            else:
+                out.write(f'"U+{code_point:04X}"\n')
---
+++ main/python/调换.py
@@ -0,0 +1,20 @@
+with open('MY1LBlocks1.yaml', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+new_lines = []
+for line in lines:
+    if "F; " in line:
+        index = line.index("F; ")
+        left = line[:index + 3]
+        right = line[index + 3:]
+        if "|" in right:
+            left_part, right_part = right.split("|")
+            new_line = left + right_part.strip() + "|" + left_part.strip() + "\n"
+            new_lines.append(new_line)
+        else:
+            new_lines.append(line)
+    else:
+        new_lines.append(line)
+
+with open('MY1LBlocks1_updated.yaml', 'w', encoding='utf-8') as file:
+    file.writelines(new_lines)
