ed2df2918d20d16551c3485e86e196c0446262b7
Losketch <140333245+Losketch@users.noreply.github.com>
2024-07-14 16:20:53 +0800

    更改生成mp4的方法

--- main/MP4.bat
+++ main/MP4.bat
@@ -1,18 +1,12 @@
 @echo off
 chcp 65001 >nul
 
-REM 1.将 GIF 转换为 MP4
-ffmpeg -i unicode_flash_mob_1.gif -movflags faststart -pix_fmt yuv420p -vf "fps=12" temp_output.mp4 -stats
+ffmpeg -i unicode_flash_mob_1.wmv -stream_loop -1 -i DUTM.ogg -c:v libx264 -crf 18 -c:a aac -b:a 192k -shortest unicode_flash_mob.mp4
 if %errorlevel% neq 0 (
     echo ffmpeg 执行失败
     pause
     exit /b
 )
-REM 2.将音频添加到 MP4
-ffmpeg -stream_loop -1 -i DUTM.ogg -i temp_output.mp4 -c:v copy -c:a aac -shortest unicode_flash_mob.mp4 -stats
-
-REM 删除临时文件
-del temp_output.mp4
 
 echo 转换完成！
 pause
